cd2d04bf2ad7eef523b7ae12f1c195c5e4bbd6cd
vkingmovies <107647005+vkingmovies@users.noreply.github.com>
2022-06-17 01:26:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7.13",
     ],
     entry_points="""
         [console_scripts]
